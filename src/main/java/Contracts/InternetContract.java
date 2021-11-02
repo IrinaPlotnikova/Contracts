ad52ae8e7216a47a2201c8db9f400918c30a6dce
Irina Plotnikova <plotnikova1029gmail.com>
2021-11-02 17:38:33 +0300

    adds a class for internet contracts

---
+++ src/main/java/Contracts/InternetContract.java
@@ -0,0 +1,52 @@
+package Contracts;
+
+import Persons.Person;
+import lombok.NonNull;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class InternetContract extends AbstractContract{
+    private int speed;
+
+    public InternetContract(int ID, int number, @NonNull LocalDate startDate, @NonNull LocalDate endDate, @NonNull Person owner, int speed) {
+        super(ID, number, startDate, endDate, owner);
+        if (speed >= 0)
+            this.speed = speed;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        if (speed >= 0)
+            this.speed = speed;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof InternetContract)) return false;
+        InternetContract that = (InternetContract) o;
+        return getID() == that.getID() && getNumber() == that.getNumber() && getSpeed() == that.getSpeed() &&
+                getStartDate() == that.getStartDate() && getEndDate() == that.getEndDate() && getOwner() == getOwner();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getID(), getNumber(), getSpeed(), getStartDate(), getEndDate(), getOwner());
+    }
+
+    @Override
+    public String toString() {
+        return "InternetContract{" +
+                "ID=" + ID +
+                ", number=" + number +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", owner=" + owner +
+                ", speed=" + speed +
+                '}';
+    }
+}
